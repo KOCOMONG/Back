6428510e082fa82bce8585733be5579efe7a1e2c
jincheol <oscar4293@gmail.com>
2022-05-30 14:13:18 +0900

    MOD app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 
 from flask import Flask
-from api import user_api,diet_api,diseasediet_api,level2_api,disease_api
+from api import user_api,diet_api,diseasediet_api,level2_api,disease_api,update_api,medicine_api
 
 def createAPP():
     app=Flask(__name__)
@@ -10,7 +10,8 @@ def createAPP():
     app.register_blueprint(diseasediet_api.bp)
     app.register_blueprint(level2_api.bp)
     app.register_blueprint(disease_api.bp)
-    
+    app.register_blueprint(update_api.bp)
+    app.register_blueprint(medicine_api.bp)
 
     return app
 
